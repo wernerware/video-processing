ea4ec200bee741445e801d6413734791f0058ddf
Daniel Werner <arachtivix@Daniels-Mac-mini.local>
2019-03-30 16:16:14 -0400

    Changed fswatch command so that it stops after one event, since the copy script can find the files just fine.  Added copy, delete, and eject lines to the copy script.

--- copy.sh
+++ copy.sh
@@ -3,4 +3,6 @@
 export toDir=`osascript -e 'tell app (path to frontmost application as Unicode text) to set new_file to POSIX path of (choose folder with prompt "Select A Folder")'  2> /dev/null`
 
 echo $toDir
-cp $0 $toDir
+cp /Volumes/MEDIA/* $toDir || { echo "FAILED TO COPY"; exit 1; }
+rm /Volumes/MEDIA/* || { echo "FAILED TO DELETE FILES"; exit 1; }
+diskutil unmount /Volumes/MEDIA
