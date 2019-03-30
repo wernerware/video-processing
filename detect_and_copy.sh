38d75e6f6122d992969727ec8548eba3536459f3
Daniel Werner <arachtivix@Daniels-Mac-mini.local>
2019-03-30 14:20:32 -0400

    Script asks for file, prints it

---
+++ detect_and_copy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export toDir=`osascript -e 'tell app (path to frontmost application as Unicode text) to set new_file to POSIX path of (choose folder with prompt "Select A Folder")'  2> /dev/null`
+
+echo $toDir
